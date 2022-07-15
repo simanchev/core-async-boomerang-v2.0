35c3079b9eb125a25a46d1d8a3098f15e18d1964
Alex <alexander.simanchev@gmail.com>
2022-07-15 12:06:14 +0300

    keyboard setup

--- src/keyboard.js
+++ src/keyboard.js
@@ -1,32 +1,20 @@
-// Умеешь работать с keypress? Попробуй разобраться в этом файле.
-// Вместо keypress можно использовать и стандартный readline.
-// Главное не используй всё вместе!
-
 const keypress = require('keypress');
 
-// Управление.
-// Настроим соответствия нажатий на клавиши и действий в игре.
-
 const keyboard = {
-  q: () => console.log('q'),
-  w: () => console.log('w'),
-  e: () => console.log('e'),
-  r: () => console.log('r'),
-  t: () => console.log('t'),
-  y: () => console.log('y'),
+  left: (hero) => hero.moveLeft(),
+  right: (hero) => hero.moveRight(),
+  up: (hero) => hero.moveUp(),
+  down: (hero) => hero.moveDown(),
+  space: (hero) => hero.attack(),
 };
 
-// Какая-то функция.
-
-function runInteractiveConsole() {
+function getKeypress() {
   keypress(process.stdin);
   process.stdin.on('keypress', (ch, key) => {
     if (key) {
-      // Вызывает команду, соответствующую нажатой кнопке.
       if (key.name in keyboard) {
         keyboard[key.name]();
       }
-      // Прерывание программы.
       if (key.ctrl && key.name === 'c') {
         process.exit();
       }
@@ -35,6 +23,4 @@ function runInteractiveConsole() {
   process.stdin.setRawMode(true);
 }
 
-// Давай попробуем запустить этот скрипт!
-
-runInteractiveConsole();
+module.exports = getKeypress;
