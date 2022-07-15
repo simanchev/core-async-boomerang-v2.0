3be4767c2961f7719c0a37170907125e8af83f92
Alex <alexander.simanchev@gmail.com>
2022-07-15 14:59:58 +0300

    registration added

--- run.js
+++ run.js
@@ -1,10 +1,20 @@
-// Основной файл.
-// Запускает игру.
+const { Sequelize } = require('sequelize');
 const Game = require('./src/Game');
 const getName = require('./src/reg');
 
+const sequelize = new Sequelize('boomteam', 'boomteam', 'boom', {
+  host: 'localhost',
+  dialect: 'postgres',
+});
+
 async function startGame() {
-  await getName();
+  const userName = await getName();
+  await sequelize.query(
+    `
+    INSERT INTO results (user_name, user_result)
+    VALUES ('${userName}', 0);
+    `,
+  );
   const game = new Game({
     trackLength: 30,
   });
